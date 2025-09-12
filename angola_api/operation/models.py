66e7f60a7ee4f583dbe17aac0a3eceb0ddaa81bf
Lulu12024 <deogratiasluc84@gmail.com>
2025-09-12 15:50:03 +0100

    update favoris

--- angola_api/operation/models.py
+++ angola_api/operation/models.py
@@ -235,8 +235,8 @@ class Favorite(TimeStampMixin):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='favorited_by')
     
-    class Meta:
-        unique_together = ('user', 'provider')
+    # class Meta:
+    #     unique_together = ('user', 'provider')
     
     def __str__(self):
         return f"{self.user.username} favorited {self.provider.user.username}"
